b24adfbca7c3015268a2c70753af6c3558b56c5e
alexandrearauj0 <alexandreearaujoo@outlook.com>
2025-08-25 21:23:31 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,9 @@
+numero =int(input("Digite o seu humor (0 a 10): "))
+if numero == 1:
+   print("To pra baixo!")
+if numero == 2:
+   print("To de boa!")
+if numero == 3:
+   print("Top de mais!")
+else:
+   print("Bom dia.")
